1c8390822fa3c5805b6fa11e34e9cc63efdc0261
sgu960323 <wjhappyboy@hanmail.net>
2020-01-09 11:08:20 +0900

    add modal & create

--- Goal/models.py
+++ Goal/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title=models.TextField(default='')
+    body=models.TextField(default='')
+    year=models.CharField(max_length=10)
+    month=models.CharField(max_length=5)
+    day=models.CharField(max_length=5)
+    reason=models.TextField(default='')
