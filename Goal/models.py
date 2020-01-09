7fea9c17cefe66fac414144b535bdddeca5f6756
sgu960323 <wjhappyboy@hanmail.net>
2020-01-09 13:51:59 +0900

    complete create

--- Goal/models.py
+++ Goal/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 class Post(models.Model):
+    author=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default='')
     title=models.TextField(default='')
     body=models.TextField(default='')
     year=models.CharField(max_length=10)
     month=models.CharField(max_length=5)
     day=models.CharField(max_length=5)
     reason=models.TextField(default='')
+    subject=models.TextField(default='')
--- Goal/views.py
+++ Goal/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render, redirect
-
+from .models import Post
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def main(request):
-    return render(request, 'main.html')
+    posts=Post.objects.all
+    if request.method=="POST":
+        author=request.user
+        title=request.POST['title']
+        body=request.POST['body']
+        year=request.POST['year']
+        month=request.POST['month']
+        day=request.POST['day']
+        reason=request.POST['reason']
+        subject=request.POST['subject']
+        Post.objects.create(author=author, title=title, body=body, year=year, month=month, day=day, reason=reason, subject=subject)
+        return redirect('main')
+    elif request.method=="GET":
+        return render(request, 'main.html', {'posts':posts})
