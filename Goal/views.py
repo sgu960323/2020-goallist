230250f4efb8d5784cf74a989965a108eed6a9e5
sgu960323 <wjhappyboy@hanmail.net>
2020-01-14 12:12:14 +0900

    add mypage delete function

--- Goal/views.py
+++ Goal/views.py
@@ -34,4 +34,30 @@ def post_detail(request):
 
 def mypage(request):
     posts=Post.objects.filter(writer=request.user)
-    return render(request, 'mypage.html', {'posts':posts})
+    if request.method=="POST":
+        author=request.user
+        writer=request.user
+        title=request.POST['title']
+        body=request.POST['body']
+        year=request.POST['year']
+        month=request.POST['month']
+        day=request.POST['day']
+        reason=request.POST['reason']
+        subject=request.POST['subject']
+        Post.objects.create(author=author, writer=writer, title=title, body=body, year=year, month=month, day=day, reason=reason, subject=subject)
+        return redirect('mypage')
+    else:
+        return render(request, 'mypage.html', {'posts':posts})
+def mydetail(request):
+    if request.method=="GET":
+        pk=request.GET['pk']
+        post=get_object_or_404(Post, pk=pk)
+        context={'author':post.writer,'title':post.title, 'body':post.body, 'year':post.year, 'month':post.month, 'day':post.day, 'reason':post.reason}
+        return JsonResponse(context)
+
+def mydelete(request):
+    if request.method=="GET":
+        pk=request.GET['pk']
+        post=get_object_or_404(Post, pk=pk)
+        post.delete();
+        return HttpResponse()
